944bb390bbeea733fc01b2f8cd1c14af1d70d77b
Viktor Snesarevskiy <snesarevskiy@yandex.ru>
2018-04-23 04:59:59 +0300

    improved code performance

--- code/Data Description.R
+++ code/Data Description.R
@@ -14,28 +14,41 @@ load("data_torque.RData")
 step <- 100
 sample <- seq(1, ncol(data_curr)-1, step)
 
-curr_downsample <- data_curr[1, sample]
-curr_downsample <- cbind(curr_downsample, data_curr[1,ncol(data_curr)])
+curr_downsample <- data_curr[, sample]
+curr_downsample <- cbind(curr_downsample, data_curr$cat)
 
-for(index in 2:nrow(data_curr)){
-  row <- data_curr[index, sample]
-  row <- cbind(row, data_curr[index,ncol(data_curr)])
-  colnames(row) <- colnames(curr_downsample)
-  
-  curr_downsample <- rbind(curr_downsample, row)
-}
+torque_downsample <- data_torque[, sample]
+torque_downsample <- cbind(torque_downsample, data_torque$cat)
 
+names(curr_downsample)[names(curr_downsample) == "data_curr$cat"] <- "cat"
+names(torque_downsample)[names(torque_downsample) == "data_torque$cat"] <- "cat"
 
-torque_downsample <- data_torque[1, sample]
-torque_downsample <- cbind(torque_downsample, data_torque[1,ncol(data_torque)])
+#---------------------------------------
+#  working vs faulty
+#---------------------------------------
+working_curr <- 0
+faulty_curr <- 0
 
-for(index in 2:nrow(data_torque)){
-  row <- data_torque[index, sample]
-  row <- cbind(row, data_curr[index,ncol(data_torque)])
-  colnames(row) <- colnames(torque_downsample)
-  
-  torque_downsample <- rbind(torque_downsample, row)
-}
+working_grasso_torque <- 0
+faulty_grasso_torque <- 0
+
+columns <- ncol(curr_downsample)
+
+working_curr <- as.vector(t(subset(curr_downsample, grepl("^working_*", cat), drop=TRUE)[1:columns-1]))
+faulty_curr <- as.vector(t(subset(curr_downsample, grepl("^faulty_*", cat), drop=TRUE)[1:columns-1]))
+
+working_torque <- as.vector(t(subset(torque_downsample, grepl("^working_*", cat), drop=TRUE)[1:columns-1]))
+faulty_torque <- as.vector(t(subset(torque_downsample, grepl("^faulty_*", cat), drop=TRUE)[1:columns-1]))
+
+mean_working_curr <- mean(working_curr)
+mean_faulty_curr <- mean(faulty_curr)
+mean_working_torque <- mean(working_torque)
+mean_faulty_torque <- mean(faulty_torque)
+
+var_working_curr <- var(working_curr)
+var_faulty_curr <- var(faulty_curr)
+var_working_torque <- var(working_torque)
+var_faulty_torque <- var(faulty_torque)
 
 #---------------------------------------
 #  working_grasso vs faulty_grasso
@@ -48,37 +61,25 @@ faulty_grasso_torque <- 0
 
 columns <- ncol(curr_downsample)
 
-for (i in 1:nrow(curr_downsample)){
-  if (curr_downsample[i, columns] == "working_grasso"){
-    working_grasso_curr <- cbind(working_grasso_curr, curr_downsample[i, 1:columns-1])
-    working_grasso_torque <- cbind(working_grasso_torque, torque_downsample[i, 1:columns-1])
-  }
-  
-  if(curr_downsample[i, columns] == "faulty_grasso"){
-    faulty_grasso_curr <- cbind(faulty_grasso_curr, curr_downsample[i, 1:columns-1])
-    faulty_grasso_torque <- cbind(faulty_grasso_torque, torque_downsample[i, 1:columns-1])
-  }
-}
+working_grasso_curr <- as.vector(t(subset(curr_downsample, grepl("^working_grasso", cat), drop=TRUE)[1:columns-1]))
+faulty_grasso_curr <- as.vector(t(subset(curr_downsample, grepl("^faulty_grasso", cat), drop=TRUE)[1:columns-1]))
 
-working_grasso_curr <- working_grasso_curr[2:length(working_grasso_curr)]
-faulty_grasso_curr <- faulty_grasso_curr[2:length(faulty_grasso_curr)]
-
-working_grasso_torque <- working_grasso_torque[2:length(working_grasso_torque)]
-faulty_grasso_torque <- faulty_grasso_torque[2:length(faulty_grasso_torque)]
+working_grasso_torque <- as.vector(t(subset(torque_downsample, grepl("^working_grasso", cat), drop=TRUE)[1:columns-1]))
+faulty_grasso_torque <- as.vector(t(subset(torque_downsample, grepl("^faulty_grasso", cat), drop=TRUE)[1:columns-1]))
 
 
 #mean
-mean_working_grasso_curr <- as.numeric(apply(working_grasso_curr,1,mean)) 
-mean_faulty_grasso_curr <- as.numeric(apply(faulty_grasso_curr,1,mean)) 
-mean_working_grasso_torque <- as.numeric(apply(working_grasso_torque,1,mean)) 
-mean_faulty_grasso_torque <- as.numeric(apply(faulty_grasso_torque,1,mean)) 
+mean_working_grasso_curr <- mean(working_grasso_curr)
+mean_faulty_grasso_curr <- mean(faulty_grasso_curr)
+mean_working_grasso_torque <- mean(working_grasso_torque)
+mean_faulty_grasso_torque <- mean(faulty_grasso_torque)
 
 
 #variance
-var_working_grasso_curr <- as.numeric(apply(working_grasso_curr,1,var)) 
-var_faulty_grasso_curr <- as.numeric(apply(faulty_grasso_curr,1,var)) 
-var_working_grasso_torque <- as.numeric(apply(working_grasso_torque,1,var)) 
-var_faulty_grasso_torque <- as.numeric(apply(faulty_grasso_torque,1,var)) 
+var_working_grasso_curr <- var(working_grasso_curr)
+var_faulty_grasso_curr <- var(faulty_grasso_curr)
+var_working_grasso_torque <- var(working_grasso_torque)
+var_faulty_grasso_torque <- var(faulty_grasso_torque)
 
 
 #-------------------------------------
@@ -260,9 +261,9 @@ var_faulty_rott2_torque <- as.numeric(apply(faulty_rott2_torque,1,var))
 library(plotly)
 
 #means of curr
-Faults <- c("grasso", "piega1", "piega2", "rott1", "rott2")
-working <- c(mean_working_grasso_curr, mean_working_piega1_curr, mean_working_piega2_curr, mean_working_rott1_curr, mean_working_rott2_curr)
-faulty <- c(mean_faulty_grasso_curr, mean_faulty_piega1_curr, mean_faulty_piega2_curr, mean_faulty_rott1_curr, mean_faulty_rott2_curr)
+Faults <- c("total", "grasso", "piega1", "piega2", "rott1", "rott2")
+working <- c(mean_working_curr, mean_working_grasso_curr, mean_working_piega1_curr, mean_working_piega2_curr, mean_working_rott1_curr, mean_working_rott2_curr)
+faulty <- c(mean_faulty_curr, mean_faulty_grasso_curr, mean_faulty_piega1_curr, mean_faulty_piega2_curr, mean_faulty_rott1_curr, mean_faulty_rott2_curr)
 data <- data.frame(Faults, working, faulty)
 
 plot_ly(data, x = ~Faults, y = ~working, type = 'bar', name = 'working', marker = list(color = 'red')) %>%
@@ -271,9 +272,9 @@ plot_ly(data, x = ~Faults, y = ~working, type = 'bar', name = 'working', marker 
 
 
 #means of torque
-Faults <- c("grasso", "piega1", "piega2", "rott1", "rott2")
-working <- c(mean_working_grasso_torque, mean_working_piega1_torque, mean_working_piega2_torque, mean_working_rott1_torque, mean_working_rott2_torque)
-faulty <- c(mean_faulty_grasso_torque, mean_faulty_piega1_torque, mean_faulty_piega2_torque, mean_faulty_rott1_torque, mean_faulty_rott2_torque)
+Faults <- c("total", "grasso", "piega1", "piega2", "rott1", "rott2")
+working <- c(mean_working_torque, mean_working_grasso_torque, mean_working_piega1_torque, mean_working_piega2_torque, mean_working_rott1_torque, mean_working_rott2_torque)
+faulty <- c(mean_faulty_torque, mean_faulty_grasso_torque, mean_faulty_piega1_torque, mean_faulty_piega2_torque, mean_faulty_rott1_torque, mean_faulty_rott2_torque)
 data <- data.frame(Faults, working, faulty)
 
 plot_ly(data, x = ~Faults, y = ~working, type = 'bar', name = 'working', marker = list(color = 'red')) %>%
